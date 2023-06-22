915143cb02ec02828c13ac45e3bccad215739af0
Eddie <Edward@veronicas-Air.attlocal.net>
2023-06-22 12:36:08 -0700

    downloaded npm and created pprompt for user

---
+++ generator.js
@@ -0,0 +1,42 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const {generateLogo} = require('./shapes.js');
+
+
+//prompt user with 3 charater length text, text color, shape, shape color
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'Enter text (3 characters max)',
+        name:'text'
+    },
+    {
+        type:'input',
+        message:'Enter text color',
+        name: 'textcolor'
+    },
+    {
+        type:'list',
+        message:'Choose the following shape for your logo',
+        name: 'list',
+        choices: [
+            {title:'square', value:'square'},
+            {title:'circle', value:'circle'},
+            {title:'triangle', value:'triangle'},
+        ],
+    },
+    {
+        type:'input',
+        message:'Enter color for selected shape',
+        name:'shapecolor',
+   },
+])
+.then((response) => {
+    logoContent = generateLogo(response);
+    fs.writeFile('logo.svg', logoContent, (err) =>
+    err ? console.error(err) : console.log('Success!logo.svg has been created')
+    )
+   
+}) 
